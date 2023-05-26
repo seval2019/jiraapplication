749cda7bfb6e6101ea8e91c2a166e3b8ccdbfc42
Seval Senturk <seval.senturk@tgs.aero>
2023-05-26 16:35:41 +0300

    sil ve güncelle

--- src/App.js
+++ src/App.js
@@ -2,42 +2,41 @@ import "./App.css";
 import TaskCreate from "./components/TaskCreate";
 import TaskList from "./components/TaskList";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const createTask =async (title, taskDesc) => {
-
-    const response=await axios.post('http://localhost:3004/tasks',{
+  const createTask = async (title, taskDesc) => {
+    const response = await axios.post("http://localhost:3004/tasks", {
       title,
-      taskDesc
+      taskDesc,
     });
-    
-    const createdTasks = [
-      ...tasks,   
-        response.data    
-    ];
+
+    const createdTasks = [...tasks, response.data];
     setTasks(createdTasks);
   };
-  const fetchTasks=async()=>{
-    const sonuc= await axios.get('http://localhost:3004/tasks');
+  const fetchTasks = async () => {
+    const sonuc = await axios.get(`http://localhost:3004/tasks`);
     setTasks(sonuc.data);
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchTasks();
-  },[])
+  }, []);
 
-  const deleteTaskById =async (id) => {
-    const sonuc= await axios.delete('http://localhost:3004/tasks/$');
+  const deleteTaskById = async (id) => {
+    await axios.delete(`http://localhost:3004/tasks/${id}`);
     const afterDeleteTask = tasks.filter((task) => {
       return task.id !== id;
     });
     setTasks(afterDeleteTask);
   };
 
-  const editTaskById = async(id, updatedTitle, updatedTaskDesc) => {
-    const sonuc= await axios.update('http://localhost:3004/tasks');
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
+    await axios.put(`http://localhost:3004/tasks/${id}`, {
+      title: updatedTitle,
+      taskDesc: updatedTaskDesc
+    });
     const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
         return { id, title: updatedTitle, taskDesc: updatedTaskDesc };
